4de95489dee32e850cb63094855d4b55262c4ddb
elenaz441 <ezasypkina03@list.ru>
2025-03-30 12:52:56 +0500

    change router order

--- src/router/v1/__init__.py
+++ src/router/v1/__init__.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter
 
 from config import settings
 
-# from .user import router as user_router
 from .auth_router import router as auth_router
 from .topic_block_router import router as topic_block_router
 from .material_router import router as material_router
@@ -16,12 +15,12 @@ from .user_router import router as user_router
 router = APIRouter(
     prefix=settings.api.v1.prefix,
 )
-router.include_router(user_router, prefix=settings.api.v1.users)
 router.include_router(auth_router, prefix=settings.api.v1.auth)
+router.include_router(user_router, prefix=settings.api.v1.users)
 router.include_router(topic_block_router, prefix=settings.api.v1.topic_block)
 router.include_router(material_router, prefix=settings.api.v1.material)
 router.include_router(feedback_router, prefix=settings.api.v1.feedback)
 router.include_router(variant_router, prefix=settings.api.v1.variant)
 router.include_router(task_router, prefix=settings.api.v1.task)
-router.include_router(homework_router, prefix=settings.api.v1.homework)
 router.include_router(class_router, prefix=settings.api.v1.classes)
+router.include_router(homework_router, prefix=settings.api.v1.homework)
